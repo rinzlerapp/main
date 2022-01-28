0d4d63060ddb996080d33788c86369634810eb72
Furkan Demir <frndemir@gmail.com>
2022-01-28 20:06:23 +0300

    :tada: let the hacking begin

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+</script>
+
+<style>
+main {
+  border: 2px dashed #e67700;
+  stroke-dashoffset: 20px;
+  height: 100%;
+}
+</style>
+
+<main>{name} is mounted!</main>
